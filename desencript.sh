2b87d5baa83c59764631d0f0deef2ebd4c8b7c95
SuSux13 <55140831+SuSux13@users.noreply.github.com>
2020-03-06 13:20:15 +0100

    Update desencript.sh

--- desencript.sh
+++ desencript.sh
@@ -1,10 +1,13 @@
+cd $HOME
+mkdir /data/data/com.termux/files/home/hehe/ver
+cd /data/data/com.termux/files/home/hehe/ver
 git clone https://github.com/SuSux13/comandosTERMUX
 echo "bash comandos.sh" > quepasa.sh
 ccryrpt -e quepasa.sh
 ccryrpt -d quepasa.sh
 
 cd comandosTERMUX
-mkdir hehe
+
 cd hehe
 
 mv comandos.sh /data/data/com.termux/files/home/hehe/ver
