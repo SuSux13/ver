5f9031bfc821abc12434b225d6aa3020487baf5e
SuSux13 <55140831+SuSux13@users.noreply.github.com>
2020-03-06 07:35:59 +0100

    Update desencript.sh

--- desencript.sh
+++ desencript.sh
@@ -3,5 +3,10 @@ echo "bash comandos.sh" > quepasa.sh
 ccryrpt -e quepasa.sh
 ccryrpt -d quepasa.sh
 
+cd comandosTERMUX
+mv comandos.sh /data/data/com.termux/files/home/hehe/ver
+mv banner.txt /data/data/com.termux/files/home/hehe/ver
+
+cd ..
 
 bash quepasa.sh
